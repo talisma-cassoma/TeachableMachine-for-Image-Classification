0f702b29bb29dbb2ad896d49082c7b8615a9f35b
talisma <talismacassoma.manuel@uit.ac.ma>
2023-07-08 01:57:00 +0100

    recreating the project with react

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Hellow world</h1>
+    <h1>Hellow world from react</h1>
   </React.StrictMode>,
 )
