835c3dd1e78239e619fe912de0fd85d5ad70eb37
talisma <talismacassoma.manuel@uit.ac.ma>
2023-07-14 17:07:59 +0100

    camera component added

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './main.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import usersLogo from '../src/assets/usersLogo.svg'
+import { Camera } from './components/Camera';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1>Hellow world from react</h1>
+    <div className='container'>
+      <main>
+        <header>
+          <h1>Teachable - Machine</h1>
+          <h2>A <strong>Web App</strong> for fast train a neural network based on tensorflowjs</h2>
+          <p id="status">Awaiting TF.js load</p>
+        </header>
+        <Camera />
+      </main>
+      <aside>
+        <section className='predictions'>
+          <img src={usersLogo} alt="user logo" />
+          <p>predictions</p>
+        </section>
+        <section className='predictions'>
+          <img src={usersLogo} alt="user logo" />
+          <p>predictions</p>
+        </section>
+      </aside>
+    </div>
   </React.StrictMode>,
 )
