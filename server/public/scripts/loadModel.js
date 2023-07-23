8a94b3d5117b8c87c55c7698b8219185ee956ee1
talisma <talismacassoma.manuel@uit.ac.ma>
2023-07-23 01:57:55 +0100

    displaying label and confidence on boudend aria

--- server/public/scripts/loadModel.js
+++ server/public/scripts/loadModel.js
@@ -14,37 +14,19 @@ let model = undefined;
 let labels = [];
 
 // Function to draw a single square on the canvas
-function drawSquare(x, y, size, color) {
+function drawSquare(x, y, size, color, text) {
 	ctx.beginPath();
-	ctx.lineWidth = "2";
-	ctx.strokeStyle = color;
 	ctx.rect(x, y, size.width, size.height);
+	ctx.strokeStyle = color;
+	ctx.lineWidth = 2;
 	ctx.stroke();
+	if (text) {
+		ctx.fillStyle = color;
+		ctx.font = '10px Arial';
+		ctx.fillText(text, x + 5, y + 10);
+	}
 }
 
-// Function to detect objects in the video frame
-async function detectObjects() {
-	const predictions = await cocoSsdModel.detect(Camera.VIDEO);
-	squares = predictions.map(prediction => ({
-		classNames: prediction.class,
-		score: `${Math.round(parseFloat(prediction.score) * 100)} % confidence`,
-		xPosition: prediction.bbox[0],
-		yPosition: prediction.bbox[1],
-		size: {
-			width: prediction.bbox[2],
-			height: prediction.bbox[3],
-		},
-		color: "red",
-	}));
-}
-
-// Function to render the detected objects on the canvas
-function render() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	squares.forEach(square => {
-		drawSquare(square.xPosition, square.yPosition, square.size, square.color);
-	});
-}
 
 // Load the Coco-SSD model
 async function loadCoco() {
@@ -61,47 +43,51 @@ const Prediction = {
 		return classNames;
 	},
 
-	// Function to set up the prediction bars based on model labels
-	async setPredictionsBars() {
-		labels = await Prediction.getModelLabelsNames();
-		labels.forEach(label => Class.createLabelPredictionsBar(label));
-	},
-
 	// Prediction loop function to continuously make predictions
-	predictLoop() {
+	async predictLoop() {
 		if (Camera.videoPlaying) {
 			ctx.drawImage(Camera.VIDEO, 0, 0, canvas.width, canvas.height);
-			detectObjects(); // Call detectObjects before rendering
-			render();
 
-			// Use Tensorflow to make predictions and update prediction bars
-			tf.tidy(function () {
-				let videoFrameAsTensor = tf.browser.fromPixels(Camera.VIDEO).div(255);
-				let resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor, [Camera.MOBILE_NET_INPUT_HEIGHT, Camera.MOBILE_NET_INPUT_WIDTH], true);
-				let imageFeatures = mobilenet.predict(resizedTensorFrame.expandDims());
-				let predict = model.predict(imageFeatures).squeeze();
-				let predictionArray = predict.arraySync();
+			const predictions = await cocoSsdModel.detect(Camera.VIDEO);
 
-				for (let i = 0; i < labels.length; i++) {
-					let classPredictionConfidence = Math.floor(predictionArray[i] * 100);
-					predictionBarsProgress[i].style.width = `${classPredictionConfidence}%`;
-					predictionBarsProgress[i].innerText = classPredictionConfidence + '%';
-				}
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+			predictions.forEach((prediction) => {
+				const imageData = ctx.getImageData(prediction.bbox[0], prediction.bbox[1], prediction.bbox[2], prediction.bbox[3]);
+
+				tf.tidy(function () {
+					let videoFrameAsTensor = tf.browser.fromPixels(imageData).div(255);
+					let resizedTensorFrame = tf.image.resizeBilinear(videoFrameAsTensor, [Camera.MOBILE_NET_INPUT_HEIGHT, Camera.MOBILE_NET_INPUT_WIDTH], true);
+					let imageFeatures = mobilenet.predict(resizedTensorFrame.expandDims());
+					let predict = model.predict(imageFeatures).squeeze();
+					let highestIndex = predict.argMax().arraySync();
+					let predictionArray = predict.arraySync();
+
+					let text = `${labels[highestIndex]}: ${Math.floor(predictionArray[highestIndex] * 100)} % confidence`;
+					drawSquare(
+						prediction.bbox[0],
+						prediction.bbox[1],
+						{
+							width: prediction.bbox[2],
+							height: prediction.bbox[3],
+						},
+						'red',
+						text // Pass the text to draw above the rectangle
+					);
+				});
 			});
 
-			// Request the next animation frame for continuous prediction loop
 			requestAnimationFrame(Prediction.predictLoop);
 		} else {
 			console.log("Camera is off");
 		}
 	},
-
 	// Function to load the AI model from the server
 	async loadModel() {
 		try {
 			model = await tf.loadLayersModel("http://localhost:3000/assets/uploads/model.json");
 			model.summary();
-			await Prediction.setPredictionsBars();
+			labels = await Prediction.getModelLabelsNames();
 			await loadCoco();
 			loadMobileNetFeatureModel();
 		} catch (error) {
